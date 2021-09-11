440bb87a959d39ab783c3baf7b18b3c6c4c288c2
Willy Jacquet <willy.jacquet@outlook.com>
2021-09-11 18:46:26 +0200

    [chaine] More data in geometry,

--- src/chaine/face_vertex_mesh/mesh/geometry.hpp
+++ src/chaine/face_vertex_mesh/mesh/geometry.hpp
@@ -7,7 +7,18 @@
 namespace chaine::face_vertex_mesh {
 
 struct Geometry {
+    std::vector<agl::Vec3> vertex_colors;
+    std::vector<float> vertex_mean_curvature;
+    std::vector<agl::Vec3> vertex_normals;
     std::vector<agl::Vec3> vertex_positions;
 };
+
+inline
+void resize_vertices(Geometry& g, std::size_t count) {
+    g.vertex_colors.resize(count, agl::vec3(1.f));
+    g.vertex_mean_curvature.resize(count, 0.f);
+    g.vertex_normals.resize(count);
+    g.vertex_positions.resize(count);
+}
 
 }
