302b14d5552c374e5a311ad8325b29b230ba8f63
Willy Jacquet <willy.jacquet@outlook.com>
2021-09-11 18:51:44 +0200

    [chaine] Vertex color and smooth shading shaders.

--- src/chaine/shader/triangle.fs
+++ src/chaine/shader/triangle.fs
@@ -1,16 +1,33 @@
 #version 450
 
+in vec3 vertex_color;
+in vec3 vertex_normal;
 in vec3 vertex_position;
 
 out vec3 fragment_rgb;
 
-void main() {
-    // Flat shading.
-    vec3 normal = normalize(cross(
+// Normal.
+
+vec3 flat_normal() {
+    return normalize(cross(
         dFdx(vertex_position),
         dFdy(vertex_position)));
+}
 
-    float lambertian = max(normal.z, 0.);
+vec3 smooth_normal() {
+    return normalize(vertex_normal);
+}
 
-    fragment_rgb = vec3(lambertian);
+//
+
+vec3 frag_color() {
+    return vertex_color;
+}
+
+void main() {
+    vec3 normal = smooth_normal();
+
+    float lambertian = abs(normal.z);
+
+    fragment_rgb = vec3(lambertian) * frag_color();
 }
--- src/chaine/shader/triangle.vs
+++ src/chaine/shader/triangle.vs
@@ -2,13 +2,19 @@
 
 uniform mat4 mvp;
 
-layout(location = 0) in vec3 POSITION;
+in vec3 COLOR;
+in vec3 NORMAL;
+in vec3 POSITION;
 
+out vec3 vertex_color;
+out vec3 vertex_normal;
 out vec3 vertex_position;
 
 void main() {
     vec4 position = mvp * vec4(POSITION, 1.);
 
+    vertex_color = COLOR;
+    vertex_normal = NORMAL;
     vertex_position = position.xyz;
 
     gl_Position = position;
