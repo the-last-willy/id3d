81d368ef52daea06ac970caabc8543e076cf39ab
Willy Jacquet <willy.jacquet@outlook.com>
2021-09-25 23:15:26 +0200

    [iehl] Replaced render pass data by programs.

---
+++ iehl/src/data/wavefront/forward_blinn_phong_program.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <agl/engine/all.hpp>
+
+namespace iehl::data::wavefront {
+
+inline
+eng::Program forward_blinn_phong_program(eng::ShaderCompiler& sc) {
+    auto p = eng::Program();
+    load(p, sc, {
+        {
+            agl::vertex_shader_tag,
+            "/wavefront/blinn_phong.vs"
+        }, {
+            agl::fragment_shader_tag,
+            "/wavefront/blinn_phong.fs"
+        }
+    });
+    p.capabilities.emplace_back(agl::Capability::blend, []() {
+        glBlendFunc(GL_ONE, GL_ONE); });
+    p.capabilities.emplace_back(agl::Capability::cull_face, []() {
+        glCullFace(GL_BACK); });
+    p.capabilities.emplace_back(agl::Capability::depth_test, []() {
+        glDepthFunc(GL_LEQUAL); });
+    return p;
+}
+
+}
