c0b52b803e10f70a1f5dae41940be59684636091
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-11 17:35:50 +0200

    [chaine] Added valid flag in vertex.

--- chaine/src/face_vertex/topology/vertex.hpp
+++ chaine/src/face_vertex/topology/vertex.hpp
@@ -7,7 +7,12 @@ namespace face_vertex {
 struct VertexTopology {
     TriangleIndex triangle;
 
+    // Both of these are the naive way of doing this.
+    // It's costs too much memory for no valid reason.
+    // Find a way that is O(1).
+    // Maybe reserving index 0 for invalid values.
     bool is_ghost = false;
+    bool is_valid = true;
 };
 
 }
