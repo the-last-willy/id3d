9816198d4d966734449ab89eae8305c63401b3c3
Devskil <33905429+Devskil@users.noreply.github.com>
2021-10-17 19:59:41 +0200

    [chaine] Circumcenter almost good
    Distance between the 2 existing points is good but not with the new added  point

--- chaine/src/face_vertex/proxy/triangle/circumcenter.hpp
+++ chaine/src/face_vertex/proxy/triangle/circumcenter.hpp
@@ -18,36 +18,22 @@ auto circumcenter(TriangleProxy tp) {
     auto bc = c - b;
     auto ca = a - c;
 
-    auto tanA = agl::cross(agl::normalize(ca), agl::normalize(ab))[2] / std::abs(agl::dot(ca, ab));
-    auto tanB = agl::cross(agl::normalize(ab), agl::normalize(bc))[2] / std::abs(agl::dot(ab, bc));
-    auto tanC = agl::cross(agl::normalize(bc), agl::normalize(ca))[2] / std::abs(agl::dot(bc, ca));
+    auto tanA = agl::cross(ca, ab)[2] / std::abs(agl::dot(ca, ab));
+    auto tanB = agl::cross(ab, bc)[2] / std::abs(agl::dot(ab, bc));
+    auto tanC = agl::cross(bc, ca)[2] / std::abs(agl::dot(bc, ca));
     
-    std:: cout << "test " << agl::cross(ca, ab)[2] << std:: endl;
-    // auto tanA = agl::length(bc) / agl::length(ac);
-    // auto tanB = agl::length(ac) / agl::length(bc);
-    // auto tanC = agl::length(ba) / agl::length(ca);
     // std::cout << "angle : ";
     // std::cout << std::abs(std::atan(tanA)) + std::abs(std::atan(tanB)) + std::abs(std::atan(tanC)) << std::endl;
 
-    return (tanB + tanC) * a + (tanC + tanA) * b + (tanA + tanB) * c;
+    auto wa = tanB + tanC;
+    auto wb = tanC + tanA;
+    auto wc = tanA + tanB;
 
-    // auto p0 = position(vertex(tp, 0));
-    // auto p1 = position(vertex(tp, 1));
-    // auto p2 = position(vertex(tp, 2));
+    auto h = (a * wa + b * wb + c * wc) / (wa + wb + wc);
 
-    // auto a = agl::length(p1 - p2);
-    // auto b = agl::length(p2 - p0);
-    // auto c = agl::length(p1 - p0);
+    std::cout << "a = " << agl::length(h - a) << " b = " << agl::length(h - b) << " c = " << agl::length(h - c) << std::endl;   
 
-    // auto wa = a * (b + c - a);
-    // auto wb = b * (c + a - b);
-    // auto wc = c * (a + b - c);
-
-    // auto h = (p0 * wa + p1 * wb + p2 * wc) / (wa + wb + wc);
-
-    // std::cout << "p0 = " << agl::length(h - p0) << " p1 = " << agl::length(h - p1) << " p2 = " << agl::length(h - p2) << std::endl;
-
-    // return (p0 * wa + p1 * wb + p2 * wc) / (wa + wb + wc);
+    return (wa * a + wb * b + wc * c) / (wa + wb + wc);
 }
 
 }
