9f77b1f5a9082122102c2aa7f07bb544474c5a2e
Devskil <33905429+Devskil@users.noreply.github.com>
2021-10-17 22:01:59 +0200

    [chaine] Working circumcenter

--- chaine/src/face_vertex/proxy/triangle/circumcenter.hpp
+++ chaine/src/face_vertex/proxy/triangle/circumcenter.hpp
@@ -18,20 +18,13 @@ auto circumcenter(TriangleProxy tp) {
     auto bc = c - b;
     auto ca = a - c;
 
-    auto tanA = agl::cross(ca, ab)[2] / std::abs(agl::dot(ca, ab));
-    auto tanB = agl::cross(ab, bc)[2] / std::abs(agl::dot(ab, bc));
-    auto tanC = agl::cross(bc, ca)[2] / std::abs(agl::dot(bc, ca));
-    
-    // std::cout << "angle : ";
-    // std::cout << std::abs(std::atan(tanA)) + std::abs(std::atan(tanB)) + std::abs(std::atan(tanC)) << std::endl;
+    auto tanA = agl::cross(ca, ab)[2] / agl::dot(ca, ab);
+    auto tanB = agl::cross(ab, bc)[2] / agl::dot(ab, bc);
+    auto tanC = agl::cross(bc, ca)[2] / agl::dot(bc, ca);
 
     auto wa = tanB + tanC;
     auto wb = tanC + tanA;
     auto wc = tanA + tanB;
-
-    auto h = (a * wa + b * wb + c * wc) / (wa + wb + wc);
-
-    std::cout << "a = " << agl::length(h - a) << " b = " << agl::length(h - b) << " c = " << agl::length(h - c) << std::endl;   
 
     return (wa * a + wb * b + wc * c) / (wa + wb + wc);
 }
