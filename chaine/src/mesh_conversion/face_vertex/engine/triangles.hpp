71a8dc5acaab2d066287bd2985fb30ca1c7c9f24
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-18 17:38:44 +0200

    [chaine] Fixed triangle mesh.

--- chaine/src/mesh_conversion/face_vertex/engine/triangles.hpp
+++ chaine/src/mesh_conversion/face_vertex/engine/triangles.hpp
@@ -12,34 +12,16 @@ namespace chaine {
 
 inline
 auto triangles_mesh(face_vertex::Mesh& m) {
-    auto index_accessor = eng::Accessor();
-    {
-        auto& accessor = index_accessor;
-
-        {
-            auto buffer_ptr = std::make_shared<eng::Buffer>();
-            auto& buffer = *buffer_ptr;
-
-            buffer.opengl = create(agl::buffer_tag);
-            storage(
-                buffer.opengl,
-                3 * triangle_count(m) * sizeof(uint32_t), 
-                GL_MAP_WRITE_BIT);
-            auto mapping = map<std::array<face_vertex::VertexIndex, 3>>(buffer.opengl, GL_WRITE_ONLY);
-            for(auto&& [i, t] : ranges::views::enumerate(triangles(m))) {
-                mapping[i] = topology(t).vertices;
-            }
-            unmap(buffer.opengl);
-
-            accessor.buffer = buffer_ptr;
-        }
-
-        accessor.buffer_view_byte_stride = agl::Stride<GLsizei>(4);
-        accessor.component_count = agl::Size<GLint>(1);
-        accessor.component_type = GL_UNSIGNED_INT;
-        
-    }
     auto primitive_ptr = std::make_shared<eng::Primitive>();
+    {
+        auto indices = std::vector<uint32_t>();
+        for(auto&& t : triangles(m)) {
+            indices.push_back(topology(t).vertices[0]);
+            indices.push_back(topology(t).vertices[1]);
+            indices.push_back(topology(t).vertices[2]);
+        }
+        primitive_ptr->indices = agl::engine::accessor(std::span(indices));
+    }
     {
         auto& primitive = *primitive_ptr;
         primitive.attributes["COLOR"] = agl::engine::accessor(
@@ -49,7 +31,7 @@ auto triangles_mesh(face_vertex::Mesh& m) {
         primitive.attributes["POSITION"] = agl::engine::accessor(
             std::span(m.geometry.vertex_positions));
         primitive.draw_type = agl::DrawType::unsigned_int;
-        primitive.indices = std::move(index_accessor);
+        // primitive.indices = std::move(index_accessor);
         primitive.primitive_count = agl::Count<GLsizei>(GLsizei(3 * triangle_count(m)));
     }
     return eng::Mesh({std::move(primitive_ptr)});
