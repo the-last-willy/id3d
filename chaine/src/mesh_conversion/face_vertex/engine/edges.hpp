a3aa8098b2b6a9c3e79e4d0cb1ed2d8f252085aa
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-18 17:47:58 +0200

    [chaine] FIxed edges mesh.

--- chaine/src/mesh_conversion/face_vertex/engine/edges.hpp
+++ chaine/src/mesh_conversion/face_vertex/engine/edges.hpp
@@ -1,39 +1,3 @@
-#pragma once
-
-#include <face_vertex/all.hpp>
-
-#include <range/v3/view/enumerate.hpp>
-
-inline
-auto edges_mesh(face_vertex::Mesh& m) {
-    auto primitive = std::make_shared<eng::Primitive>();
-    {
-        primitive->draw_mode = agl::DrawMode::lines;
-        primitive->draw_type = agl::DrawType::unsigned_int;
-    }
-    {
-        auto positions = std::vector<agl::Vec3>();
-        for(auto&& t : triangles(m)) {
-            if(not is_ghost(t)) {
-                positions.push_back(position(vertex(t, 0)));
-                positions.push_back(position(vertex(t, 1)));
-
-                positions.push_back(position(vertex(t, 1)));
-                positions.push_back(position(vertex(t, 2)));
-
-                positions.push_back(position(vertex(t, 2)));
-                positions.push_back(position(vertex(t, 0)));
-            }
-        }
-
-        primitive->attributes["POSITION"]
-        = agl::engine::accessor(std::span(positions));
-        primitive->primitive_count
-        = agl::Count<GLsizei>(static_cast<GLsizei>(size(positions)));
-    }
-    return eng::Mesh({std::move(primitive)});
-}
-
 // #pragma once
 
 // #include <face_vertex/all.hpp>
@@ -48,24 +12,60 @@ auto edges_mesh(face_vertex::Mesh& m) {
 //         primitive->draw_type = agl::DrawType::unsigned_int;
 //     }
 //     {
-//         auto indices = std::vector<agl::Vec3>();
+//         auto positions = std::vector<agl::Vec3>();
 //         for(auto&& t : triangles(m)) {
-//             indices.push_back(vertex(t, 0));
-//             indices.push_back(vertex(t, 1));
+//             if(not is_ghost(t)) {
+//                 positions.push_back(position(vertex(t, 0)));
+//                 positions.push_back(position(vertex(t, 1)));
 
-//             positions.push_back(vertex(t, 1));
-//             positions.push_back(vertex(t, 2));
+//                 positions.push_back(position(vertex(t, 1)));
+//                 positions.push_back(position(vertex(t, 2)));
 
-//             positions.push_back(vertex(t, 2));
-//             positions.push_back(vertex(t, 0));
+//                 positions.push_back(position(vertex(t, 2)));
+//                 positions.push_back(position(vertex(t, 0)));
+//             }
 //         }
 
-//         primitive->
-
 //         primitive->attributes["POSITION"]
-//         = agl::engine::accessor(std::span(m.geometry.vertex_positions));
+//         = agl::engine::accessor(std::span(positions));
 //         primitive->primitive_count
 //         = agl::Count<GLsizei>(static_cast<GLsizei>(size(positions)));
 //     }
 //     return eng::Mesh({std::move(primitive)});
 // }
+
+#pragma once
+
+#include <face_vertex/all.hpp>
+
+#include <range/v3/view/enumerate.hpp>
+
+inline
+auto edges_mesh(face_vertex::Mesh& m) {
+    auto primitive = std::make_shared<eng::Primitive>();
+    {
+        primitive->draw_mode = agl::DrawMode::lines;
+        primitive->draw_type = agl::DrawType::unsigned_int;
+    }
+    {
+        auto indices = std::vector<uint32_t>();
+        for(auto&& t : triangles(m)) {
+            indices.push_back(index(vertex(t, 0)));
+            indices.push_back(index(vertex(t, 1)));
+
+            indices.push_back(index(vertex(t, 1)));
+            indices.push_back(index(vertex(t, 2)));
+
+            indices.push_back(index(vertex(t, 2)));
+            indices.push_back(index(vertex(t, 0)));
+        }
+
+        primitive->indices = agl::engine::accessor(std::span(indices));
+
+        primitive->attributes["POSITION"]
+        = agl::engine::accessor(std::span(m.geometry.vertex_positions));
+        primitive->primitive_count
+        = agl::Count<GLsizei>(static_cast<GLsizei>(size(indices)));
+    }
+    return eng::Mesh({std::move(primitive)});
+}
