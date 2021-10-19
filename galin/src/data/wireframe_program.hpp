d01990379a536d74c9aa691b26d939cabbce65b5
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-19 23:23:48 +0200

    [galin] Incomplete wireframe program.

---
+++ galin/src/data/wireframe_program.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <agl/engine/all.hpp>
+
+namespace data {
+
+inline
+auto wireframe_program(eng::ShaderCompiler& sc) {
+    auto p = eng::Program();
+    load(p, sc, {
+        {
+            agl::vertex_shader_tag,
+            "/mesh/wireframe.vert"
+        }, {
+            agl::fragment_shader_tag,
+            "/mesh/wireframe.frag"
+        }
+    });
+    p.capabilities.emplace_back(agl::Capability::cull_face, []() {
+        glCullFace(GL_BACK); });
+    p.capabilities.emplace_back(agl::Capability::depth_test, []() {
+        glDepthFunc(GL_LEQUAL); });
+    return p;
+}
+
+}
