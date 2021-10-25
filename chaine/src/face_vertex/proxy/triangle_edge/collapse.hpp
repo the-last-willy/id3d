aab954f540903381e7a176ead700686bcd28fa89
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-25 18:44:11 +0200

    [chaine] Collapse debug prints.

--- chaine/src/face_vertex/proxy/triangle_edge/collapse.hpp
+++ chaine/src/face_vertex/proxy/triangle_edge/collapse.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "adjacent_triangle.hpp"
+#include "can_collapse.hpp"
 #include "proxy.hpp"
 
 #include "face_vertex/proxy/triangle/adjacent_triangle_after.hpp"
@@ -11,7 +12,7 @@
 #include "face_vertex/proxy/triangle/vertex_after.hpp"
 #include "face_vertex/proxy/vertex/invalidate.hpp"
 #include "face_vertex/proxy/vertex/proxy.hpp"
-#include "face_vertex/proxy/vertex/substitute.hpp"
+#include "face_vertex/proxy/vertex/substitute_by.hpp"
 
 #include <iostream>
 
@@ -23,16 +24,14 @@ namespace face_vertex {
 // Returns collapsed vertex.
 inline
 VertexProxy collapse(const TriangleEdgeProxy& tep) {
-    std::cout << "collpase " << tep.index.value[0] << " " << tep.index.value[1] << std::endl;
+    // std::cout << "collpase " << tep.index.value[0] << " " << tep.index.value[1] << " " << can_collapse(tep) << std::endl;
 
-    std::cout << "BEFORE" << std::endl;
+
+    // std::cout << "BEFORE" << std::endl;
 
     auto t0 = adjacent_triangle(tep, 0);
     auto t1 = adjacent_triangle(tep, 1);
-
-    std::cout << "t0: " << string(t0);
-    std::cout << "t1: " << string(t1);
-
+    
     auto va = vertex_after(t0, opposite_vertex(t0, t1));
     auto vb = vertex_after(t1, opposite_vertex(t1, t0));
 
@@ -41,9 +40,18 @@ VertexProxy collapse(const TriangleEdgeProxy& tep) {
 
     auto t1a = adjacent_triangle_after(t1, t0);
     auto t1b = adjacent_triangle_after(t1, t1a);
+
+    // std::cout << "t0: " << string(t0);
+    // std::cout << "t1: " << string(t1);
+
+    // std::cout << "t0a: " << string(t0a);
+    // std::cout << "t0b: " << string(t0b);
+
+    // std::cout << "t1a: " << string(t1a);
+    // std::cout << "t1b: " << string(t1b);
     
     { // Remove vb.
-        substitute(vb, va);
+        substitute_by(vb, va);
         invalidate(vb);
     }
     { // Sew t0 faces.
@@ -61,10 +69,16 @@ VertexProxy collapse(const TriangleEdgeProxy& tep) {
         invalidate(t1);
     }
 
-    std::cout << "AFTER" << std::endl;
+    // std::cout << "AFTER" << std::endl;
 
-    std::cout << "t0: " << string(t0);
-    std::cout << "t1: " << string(t1);
+    // std::cout << "t0: " << string(t0);
+    // std::cout << "t1: " << string(t1);
+
+    // std::cout << "t0a: " << string(t0a);
+    // std::cout << "t0b: " << string(t0b);
+
+    // std::cout << "t1a: " << string(t1a);
+    // std::cout << "t1b: " << string(t1b);
     
     return va;
 }
