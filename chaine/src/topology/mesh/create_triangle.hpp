441d9aa0b4bc4dfef73b0353695bff4eaa536d23
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-26 15:37:20 +0200

    [chaine] Topology creation returns a proxy.

--- chaine/src/topology/mesh/create_triangle.hpp
+++ chaine/src/topology/mesh/create_triangle.hpp
@@ -1,18 +1,19 @@
 #pragma once
 
+#include "triangle/proxy.hpp"
 #include "topology.hpp"
 
 namespace face_vertex {
 
 inline
-TriangleIndex create_triangle(Topology& t) {
+TriangleTopologyProxy create_triangle(Topology& t) {
     auto ti = TriangleIndex(static_cast<uint32_t>(size(t.triangles)));
     t.triangle_count += 1;
     auto&& tr = t.triangles.emplace_back();
     for(auto& at : tr.triangles) {
         at = ti;
     }
-    return ti;
+    return proxy(t, ti);
 }
 
 }
--- chaine/src/topology/mesh/create_vertex.hpp
+++ chaine/src/topology/mesh/create_vertex.hpp
@@ -1,14 +1,16 @@
 #pragma once
 
+#include "vertex/proxy.hpp"
 #include "topology.hpp"
 
 namespace face_vertex {
 
 inline
-VertexIndex create_vertex(Topology& t) {
-    auto i = static_cast<uint32_t>(size(t.vertices));
+VertexTopologyProxy create_vertex(Topology& t) {
+    auto vi = VertexIndex(static_cast<uint32_t>(size(t.vertices)));
     t.vertex_count += 1;
-    return VertexIndex(i);
+    t.vertices.emplace_back();
+    return proxy(t, vi);
 }
 
 }
