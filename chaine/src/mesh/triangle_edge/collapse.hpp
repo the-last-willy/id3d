6f4af0522e369e370a66b26b3050850f60b17438
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-26 19:33:35 +0200

    [chaine] Removed debug prints.

--- chaine/src/mesh/triangle_edge/collapse.hpp
+++ chaine/src/mesh/triangle_edge/collapse.hpp
@@ -24,13 +24,13 @@ namespace face_vertex {
 // Returns collapsed vertex.
 inline
 VertexProxy collapse(const TriangleEdgeProxy& tep) {
-    std::cout << "collapse " << tep.index.value[0] << " " << tep.index.value[1] << " " << can_collapse(tep) << std::endl;
+    // std::cout << "collapse " << tep.index.value[0] << " " << tep.index.value[1] << " " << can_collapse(tep) << std::endl;
 
     auto t0 = adjacent_triangle(tep, 0);
     auto t1 = adjacent_triangle(tep, 1);
 
-    std::cout << "0: " << index(opposite_vertex(t0, t1)) << std::endl;
-    std::cout << "1: " << index(opposite_vertex(t1, t0)) << std::endl;
+    // std::cout << "0: " << index(opposite_vertex(t0, t1)) << std::endl;
+    // std::cout << "1: " << index(opposite_vertex(t1, t0)) << std::endl;
     
     auto va = vertex_after(t0, opposite_vertex(t0, t1));
     auto vb = vertex_after(t1, opposite_vertex(t1, t0));
@@ -41,16 +41,16 @@ VertexProxy collapse(const TriangleEdgeProxy& tep) {
     auto t1a = adjacent_triangle_after(t1, t0);
     auto t1b = adjacent_triangle_after(t1, t1a);
 
-    std::cout << "BEFORE" << std::endl;
+    // std::cout << "BEFORE" << std::endl;
 
-    std::cout << "t0: " << string(t0);
-    std::cout << "t1: " << string(t1);
+    // std::cout << "t0: " << string(t0);
+    // std::cout << "t1: " << string(t1);
 
-    std::cout << "t0a: " << string(t0a);
-    std::cout << "t0b: " << string(t0b);
+    // std::cout << "t0a: " << string(t0a);
+    // std::cout << "t0b: " << string(t0b);
 
-    std::cout << "t1a: " << string(t1a);
-    std::cout << "t1b: " << string(t1b);
+    // std::cout << "t1a: " << string(t1a);
+    // std::cout << "t1b: " << string(t1b);
     
     { // Remove vb.
         substitute_by(vb, va);
@@ -79,16 +79,16 @@ VertexProxy collapse(const TriangleEdgeProxy& tep) {
         // std::cout << "t1b: " << is_valid(vertex_after(t1b, va)) << std::endl;
     }
 
-    std::cout << "AFTER" << std::endl;
+    // std::cout << "AFTER" << std::endl;
 
-    std::cout << "t0: " << string(t0);
-    std::cout << "t1: " << string(t1);
+    // std::cout << "t0: " << string(t0);
+    // std::cout << "t1: " << string(t1);
 
-    std::cout << "t0a: " << string(t0a);
-    std::cout << "t0b: " << string(t0b);
+    // std::cout << "t0a: " << string(t0a);
+    // std::cout << "t0b: " << string(t0b);
 
-    std::cout << "t1a: " << string(t1a);
-    std::cout << "t1b: " << string(t1b);
+    // std::cout << "t1a: " << string(t1a);
+    // std::cout << "t1b: " << string(t1b);
     
     return va;
 }
