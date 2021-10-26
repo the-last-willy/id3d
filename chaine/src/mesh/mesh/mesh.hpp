5b9c874c3eace10547b4a3ea36a35065e9e87a31
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-26 19:36:06 +0200

    [chaine] New vertex count.

--- chaine/src/mesh/mesh/mesh.hpp
+++ chaine/src/mesh/mesh/mesh.hpp
@@ -14,10 +14,7 @@ uint32_t triangle_count(const Mesh& m) {
     return static_cast<uint32_t>(size(m.topology.triangles));
 }
 
-inline
-uint32_t vertex_count(const Mesh& m) {
-    return static_cast<uint32_t>(size(m.topology.vertices));
-}
+
 
 inline
 Geometry& geometry(Mesh& m) {
--- chaine/src/mesh/mesh/vertex_count.hpp
+++ chaine/src/mesh/mesh/vertex_count.hpp
@@ -1,21 +1,13 @@
 #pragma once
 
 #include "mesh.hpp"
-#include "vertices.hpp"
-
-#include "mesh/vertex/is_valid.hpp"
+#include "topology.hpp"
 
 namespace face_vertex {
 
-// inline
-// uint32_t vertex_count(Mesh& m) {
-//     auto count = uint32_t(0);
-//     for(auto&& v : vertices(m)) {
-//         if(is_valid(v)) {
-//             count += 1;
-//         }
-//     }
-//     return count;
-// }
+inline
+uint32_t vertex_count(Mesh& m) {
+    return vertex_count(topology(m));
+}
 
 }
