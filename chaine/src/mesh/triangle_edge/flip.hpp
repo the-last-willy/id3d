2ee2f921b59ba40ee970d0cacd90328392420ff4
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-29 22:58:41 +0200

    [chaine] Fixed flip without ghost vertex.

--- chaine/src/mesh/triangle_edge/flip.hpp
+++ chaine/src/mesh/triangle_edge/flip.hpp
@@ -10,16 +10,36 @@ namespace face_vertex {
 
 inline
 void flip(TriangleEdgeProxy tep) {
-    auto at0 = adjacent_triangle(tep, 0);
-    auto at1 = adjacent_triangle(tep, 1);
-    auto ata0 = adjacent_triangle_after(at0, at1);
-    auto ata1 = adjacent_triangle_after(at1, at0);
-    substitute_adjacent_triangle(at0, at1, ata1);
-    substitute_adjacent_triangle(at0, ata0, at1);
-    substitute_adjacent_triangle(at1, at0, ata0);
-    substitute_adjacent_triangle(at1, ata1, at0);
-    substitute_adjacent_triangle(ata0, at0, at1);
-    substitute_adjacent_triangle(ata1, at1, at0);
+    auto t0 = adjacent_triangle(tep, 0);
+    auto t1 = adjacent_triangle(tep, 1);
+
+    auto t0_t1_i = relative_index(t0, t1);
+    auto t0_at0_i = (t0_t1_i + 1) % 3;
+    auto t1_t0_i = relative_index(t1, t0);
+    auto t1_at1_i = (t1_t0_i + 1) % 3;
+
+    auto at0 = adjacent_triangle(t0, t0_at0_i);
+    auto at1 = adjacent_triangle(t1, t1_at1_i);
+
+    auto tt0 = topology(t0);
+    auto tt1 = topology(t1);
+
+    {
+        tt0->vertices[(t0_t1_i + 2) % 3] = tt1->vertices[t1_t0_i];
+        tt1->vertices[(t1_t0_i + 2) % 3] = tt0->vertices[t0_t1_i];
+    }
+
+    topology(t0)->triangles[t0_at0_i] = index(t1);
+    topology(t1)->triangles[t1_at1_i] = index(t0);
+
+    if(index(at0) != index(t0)) {
+        topology(t1)->triangles[t1_t0_i] = index(at0);
+        substitute_adjacent_triangle(at0, t0, t1);
+    }
+    if(index(at1) != index(t1)) {
+        topology(t0)->triangles[t0_t1_i] = index(at1);
+        substitute_adjacent_triangle(at1, t1, t0);
+    }
 }
 
 }
