07005f4a671987abf86c3878fdbd8fcd65ccdc27
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-29 22:59:35 +0200

    [chaine] Cleaned orientation predicates.

--- chaine/src/predicate/orientation.hpp
+++ chaine/src/predicate/orientation.hpp
@@ -3,8 +3,21 @@
 #include <agl/opengl/all.hpp>
 
 inline
-float orientation(agl::Vec2 v0, agl::Vec2 v1, agl::Vec2 v2) {
-    auto d1 = agl::vec3(v1 - v0, 0.f);
-    auto d2 = agl::vec3(v2 - v0, 0.f);
-    return cross(d1, d2)[2];
+bool orientation(agl::Vec2 v0, agl::Vec2 v1) {
+    return cross(v0, v1) > 0.f;
+}
+
+inline
+bool orientation(agl::Vec2 origin, agl::Vec2 p0, agl::Vec2 p1) {
+    return orientation(p0 - origin, p1 - origin);
+}
+
+inline
+bool orientation(agl::Vec3 v0, agl::Vec3 v1, agl::Vec3 v2) {
+    return dot(cross(v0, v1), v2) > 0.f;
+}
+
+inline
+float orientation(agl::Vec3 origin, agl::Vec3 p0, agl::Vec3 p1, agl::Vec3 p2) {
+    return orientation(p0 - origin, p1 - origin, p2 - origin);
 }
