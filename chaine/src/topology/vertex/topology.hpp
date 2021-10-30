2e6ab3abdef571ca2dca2c3bef03aa53f940057f
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-30 13:16:41 +0200

    [chaine] Removed obsolete comment.

--- chaine/src/topology/vertex/topology.hpp
+++ chaine/src/topology/vertex/topology.hpp
@@ -7,13 +7,6 @@ namespace face_vertex {
 struct VertexTopology {
     TriangleIndex triangle;
 
-    // Both of these are the naive way of doing this.
-    // It's costs too much memory for no valid reason.
-    // Find a way that is O(1).
-    // Maybe reserving index 0 or max_val for invalid values.
-    // bool is_ghost = false;
-    // bool is_valid = true;
-
     // TriangleIndex adjacent_triangle;
 };
 
