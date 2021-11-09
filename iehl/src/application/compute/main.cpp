da8478fcb8be84cdcda25087af2a07d7fc662a49
Willy Jacquet <willy.jacquet@outlook.com>
2021-11-09 11:21:12 +0100

    [iehl] TP4.

--- iehl/src/application/compute/main.cpp
+++ iehl/src/application/compute/main.cpp
@@ -24,8 +24,50 @@
 //
 
 struct App : Program {
+    eng::ShaderCompiler compiler;
+
     void init() override {
+        auto v = std::vector<int>(10);
+        std::iota(begin(v), end(v), 0);
+        for(auto e : v) {
+            std::cout << e << " ";
+        }
+        std::cout << std::endl;
+
+        auto p = eng::Program();
+        load(p, compiler, {{
+            agl::compute_shader_tag,
+            "iehl/src/application/compute/filter.comp"
+        }});
+        bind(p);
+
+        auto input_b = create(agl::buffer_tag);
+        storage(input_b, std::span(v));
+
+        auto output_b = create(agl::buffer_tag);
+        storage<int>(output_b, size(v));
+
+        auto counter_b = create(agl::buffer_tag);
+        storage<int>(counter_b, 1);
+
+        glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, input_b);
+        glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, output_b);
+        glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, counter_b);
+        // glUniform1i(0, 12);
+
+        glDispatchCompute((size(v) + 7) / 8, 1, 1);
+        glMemoryBarrier(GL_ALL_BARRIER_BITS);
+
+        glGetNamedBufferSubData(output_b, 0, agl::standard::byte_size(v), data(v));
+
+        auto count = 0;
+        glGetNamedBufferSubData(counter_b, 0, 4, &count);
         
+        std::cout << "Count: " << count << std::endl;
+        for(auto e : v) {
+            std::cout << e << " ";
+        }
+        std::cout << std::endl;
     }
 
     void update(float) override {
