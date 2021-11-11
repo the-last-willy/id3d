92dc11b7aab2585999c0545d5c83fa3ba812c2fd
Willy Jacquet <willy.jacquet@outlook.com>
2021-11-11 21:46:27 +0100

    [iehl] Removed gizmos.

--- iehl/src/data/all.hpp
+++ iehl/src/data/all.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "gizmo/all.hpp"
 #include "shader/all.hpp"
 #include "wavefront/all.hpp"
 #include "uniform_texture.hpp"
--- iehl/src/data/gizmo/all.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include "bounding_box.hpp" 
