8d72ea9f0e899a0b8b7bee17093397d66822ef54
Yoan Parra <yoan.parra38@gmail.com>
2021-11-19 10:47:37 +0100

    [galin/terrain] hill slope erosion

---
+++ galin/src/terrain/erosion/hill_slope.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "terrain/all.hpp"
+
+inline 
+void hill_slope_erosion(Terrain &t) {
+    auto nx = resolution(t)[0];
+    auto ny = resolution(t)[1];
+    for(size_t i = 0; i < nx; ++i)
+    for(size_t j = 0; j < ny; ++j) {
+        auto laplacien = at(t.laplaciens, i, j);
+        at(t.heights, i, j) += -0.01f * laplacien;
+    }
+}
