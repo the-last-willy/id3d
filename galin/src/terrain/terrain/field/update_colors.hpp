f8112952029bd4f3ac75b5523d54bbe7a998704f
Willy Jacquet <willy.jacquet@outlook.com>
2021-11-22 19:28:34 +0100

    [galin/terrain] Better Laplacian colors.

--- galin/src/terrain/terrain/field/update_colors.hpp
+++ galin/src/terrain/terrain/field/update_colors.hpp
@@ -7,7 +7,11 @@ inline
 void update_colors(Terrain& t) {
     for(std::size_t i = 0; i < resolution(t)[0]; ++i)
     for(std::size_t j = 0; j < resolution(t)[1]; ++j) {
+        auto l = at(t.laplacian, i, j);
         at(t.colors, i, j)
-        = agl::vec3(1 / (at(t.laplacian, i, j) + 1));
+        = agl::vec3(
+            std::min(std::max(l, 0.f), 1.f),
+            0.f,
+            std::min(std::max(-l, 0.f), 1.f));
     }
 }
