961bd77c9fa55622113708e1c9ae35048269e8e2
Willy Jacquet <willy.jacquet@outlook.com>
2021-11-27 21:24:54 +0100

    [iehl] App settings.

--- iehl/src/application/rasterization/settings/all.hpp
+++ iehl/src/application/rasterization/settings/all.hpp
@@ -1,13 +1,3 @@
 #pragma once
 
-#include "debugging.hpp"
-#include "frustrum_culling.hpp"
-#include "optimization.hpp"
-#include "ui.hpp"
-
-struct AllSettings {
-    DebuggingSettings debugging;
-    FrustrumCullingSettings frustrum_culling;
-    OptimizationSettings optimization;
-    UiSettings ui;
-};
+#include "settings.hpp"
---
+++ iehl/src/application/rasterization/settings/settings.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+struct Settings {
+    bool rasterization_enabled = true;  
+
+    bool bvh_debugging_enabled = false;
+    int bvh_debugging_level = 0;
+
+    bool object_grid_debugging_enabled = false;
+};
