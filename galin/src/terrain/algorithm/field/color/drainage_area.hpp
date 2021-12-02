f2aa8f445460c676627df9d4f28b29f263437be0
Willy Jacquet <willy.jacquet@outlook.com>
2021-12-02 20:41:25 +0100

    [galin] Better drainage area coloration.

--- galin/src/terrain/algorithm/field/color/drainage_area.hpp
+++ galin/src/terrain/algorithm/field/color/drainage_area.hpp
@@ -7,8 +7,15 @@ void update_colors_using_drainage_area(Terrain& t) {
     auto c = value_accessor(t.color);
     auto da = value_accessor(t.drainage_area);
 
+    auto compression = [](float f) {
+        return std::log(f);
+    };
+
+    auto max = upper_bound(t.drainage_area.range);
+    auto extent = compression(max);
+
     for(std::size_t i = 0; i < resolution(t)[0]; ++i)
     for(std::size_t j = 0; j < resolution(t)[1]; ++j) {
-        c(i, j) = agl::vec3(1.f / (da(i, j) / 10.f + 1.f));
+        c(i, j) = agl::vec3(0.f, 0.f, compression(da(i, j)) / extent);
     }
 }
