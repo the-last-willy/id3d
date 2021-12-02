2bb0119651fcacd3d299efc70a4fd98f1c550349
Willy Jacquet <willy.jacquet@outlook.com>
2021-12-02 20:41:39 +0100

    [galin] Better Laplacian coloration.

--- galin/src/terrain/algorithm/field/color/laplacian.hpp
+++ galin/src/terrain/algorithm/field/color/laplacian.hpp
@@ -2,17 +2,34 @@
 
 #include "terrain/all.hpp"
 
+#include <cmath>
+
 inline
 void update_colors_using_laplacian(Terrain& t) {
     auto c = value_accessor(t.color);
     auto l = value_accessor(t.laplacian);
 
+    auto compressed = [](float f) {
+        return std::sqrt(std::abs(f) + 1.f) - 1.f;
+    };
+
+    auto min = lower_bound(t.laplacian.range);
+    auto max = upper_bound(t.laplacian.range);
+    auto extent = std::max(compressed(min), compressed(max));
+
     for(std::size_t i = 0; i < resolution(t)[0]; ++i)
     for(std::size_t j = 0; j < resolution(t)[1]; ++j) {
         auto l_ij = l(i, j);
-        c(i, j) = agl::vec3(
-            std::max(  l_ij, 0.f),
-            0.f,
-            std::max(- l_ij, 0.f));
+        auto cl = compressed(l_ij);
+
+        auto r = 0.f;
+        auto b = 0.f;
+        if(l_ij > 0.f) {
+            r = cl / extent;
+        } else if(l_ij < 0.f) {
+            b = cl / extent;
+        }
+
+        c(i, j) = agl::vec3(r, 0.f, b);
     }
 }
