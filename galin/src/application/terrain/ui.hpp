9c973898b884198038580313dd227eb21260a776
Willy Jacquet <willy.jacquet@outlook.com>
2021-12-03 19:46:45 +0100

    [galin] Breaching UI.

--- galin/src/application/terrain/ui.hpp
+++ galin/src/application/terrain/ui.hpp
@@ -4,30 +4,40 @@
 
 void App::ui() {
     if(ImGui::Begin("Settings")) {
-        if(ImGui::TreeNode("Erosion")) {
-            if(ImGui::TreeNode("Debris slope")) {
-                ImGui::SliderFloat("Factor",
-                    &settings.debris_slope_erosion_factor, 0.f, 5.f);
-                if(ImGui::Button("Erode")) {
-                    debris_slope_erosion(terrain);
-                    settings.is_height_outdated = true;
+        if(ImGui::TreeNode("Algorithm")) {
+            if(ImGui::TreeNode("Erosion")) {
+                if(ImGui::TreeNode("Debris slope")) {
+                    ImGui::SliderFloat("Factor",
+                        &settings.debris_slope_erosion_factor, 0.f, 5.f);
+                    if(ImGui::Button("Erode")) {
+                        debris_slope_erosion(terrain);
+                        settings.is_height_outdated = true;
+                    }
+                    ImGui::TreePop();
+                }
+                if(ImGui::TreeNode("Hill slope")) {
+                    ImGui::SliderFloat("Factor",
+                        &settings.hill_slope_erosion_factor, 0.f, 5.f);
+                    if(ImGui::Button("Erode")) {
+                        hill_slope_erosion(terrain);
+                        settings.is_height_outdated = true;
+                    }
+                    ImGui::TreePop();
+                }
+                if(ImGui::TreeNode("Stream power")) {
+                    ImGui::SliderFloat("Factor",
+                        &settings.stream_power_erosion_factor, 0.f, 5.f);
+                    if(ImGui::Button("Erode")) {
+                        stream_power_erosion(terrain);
+                        settings.is_height_outdated = true;
+                    }
+                    ImGui::TreePop();
                 }
                 ImGui::TreePop();
             }
-            if(ImGui::TreeNode("Hill slope")) {
-                ImGui::SliderFloat("Factor",
-                    &settings.hill_slope_erosion_factor, 0.f, 5.f);
-                if(ImGui::Button("Erode")) {
-                    hill_slope_erosion(terrain);
-                    settings.is_height_outdated = true;
-                }
-                ImGui::TreePop();
-            }
-            if(ImGui::TreeNode("Stream power")) {
-                ImGui::SliderFloat("Factor",
-                    &settings.stream_power_erosion_factor, 0.f, 5.f);
-                if(ImGui::Button("Erode")) {
-                    stream_power_erosion(terrain);
+            if(ImGui::TreeNode("Breaching")) {
+                if(ImGui::Button("Breach")) {
+                    breaching::breach(terrain);
                     settings.is_height_outdated = true;
                 }
                 ImGui::TreePop();
