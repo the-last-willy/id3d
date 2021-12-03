d01d8c2665bd10783112ce3afea17914e329e6db
Willy Jacquet <willy.jacquet@outlook.com>
2021-12-03 13:13:34 +0100

    [galin] Compiling breaching.

--- galin/src/terrain/algorithm/breaching/all.hpp
+++ galin/src/terrain/algorithm/breaching/all.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
-#include "terrain/all.hpp"
+#include "terrain/resolution.hpp"
+#include "terrain/terrain.hpp"
 
+#include <limits>
 #include <queue>
+#include <utility>
 
 namespace breaching {
 
@@ -21,8 +24,9 @@ struct comp {
 	const Point& lhs = a.second;
 	const Point& rhs = a.second;
 
-	if(a.first > b.first) return true;
-	else if((a.first == b.first) && (lhs[0] > rhs[0] || (lhs[0] == rhs[0] && lhs[1] > rhs[1])))
+	if(a.first > b.first) {
+		return true;
+	} else if((a.first == b.first) && (lhs[0] > rhs[0] || (lhs[0] == rhs[0] && lhs[1] > rhs[1])))
 	  return true;
 	else return false;
   }
@@ -35,13 +39,6 @@ enum LindsayMode {
   SELECTIVE_BREACHING,
   CONSTRAINED_BREACHING
 };
-
-enum LindsayCellType {
-  UNVISITED,
-  VISITED,
-  EDGE
-};
-
 
 /*!
 \brief Breach depressions.
@@ -61,38 +58,39 @@ See https://github.com/r-barnes/richdem/blob/master/include/richdem/depressions/
 void breach(Terrain& t) {
 	auto h = value_accessor(t.height);
 
-	int mode = COMPLETE_BREACHING;
-	bool fill_depressions = false;
+	auto nx = resolution(t)[0];
+	auto ny = resolution(t)[1];
 
-	// Heightfield properties
-	const Box2 box = Array2::GetBox();
+	auto backlinks = agl::common::grid<int>(nx, ny);
+	for(auto& b : backlinks) {
+		b = -1;
+	}
+	auto pits = agl::common::grid<char>(nx, ny);
+	for(auto it = pits.begin(); it != pits.end(); ++it) {
+		*it = false;
+	}
+	auto visited = agl::common::grid<char>(nx, ny);
+	for(auto it = visited.begin(); it != visited.end(); ++it) {
+		*it = false;
+	}
 
-	const int NO_BACK_LINK = -1;
-
-	ArrayInteger2 backlinks(box, nx, ny, NO_BACK_LINK);
-	ArrayInteger2 visited(box, nx, ny, LindsayCellType::UNVISITED);
-	ArrayInteger2 pits(box, nx, ny, false);
-
-	std::vector<int> flood_array;
 	ZPriorityQueue pq;
 
-	int total_pits = 0;
-
 	// Seed the priority queue
-	for(int x = 0; x < nx; x++)
-	for(int y = 0; y < ny; y++) {
+	for(std::size_t x = 0; x < nx; x++)
+	for(std::size_t y = 0; y < ny; y++) {
 		// Valid edge cells go on priority-queue
 		if(x == 0 || x == (nx - 1) || y == 0 || y == (ny - 1)) {
-			pq.emplace(h(x, y), Point(x, y));
-			visited(x, y) = LindsayCellType::EDGE;
+			pq.emplace(std::make_pair(h(x, y), std::array<std::size_t, 2>{x, y}));
+			at(visited, x, y) = true;
 			continue;
 		}
 
 		// Determine if this is an edge cell, gather information used to determine if it is a pit cell
 		float lowest_neighbour = std::numeric_limits<float>::max();
-		for(int n = 0; n < DIRECTIONS; n++) {
-			const int px = x + dx[n];
-			const int py = y + dy[n];
+		for(std::size_t n = 0; n < DIRECTIONS; n++) {
+			auto px = x + dx[n];
+			auto py = y + dy[n];
 
 			// No need for an inGrid check here because edge cells are filtered above
 			// Used for identifying the lowest neighbour
@@ -103,79 +101,55 @@ void breach(Terrain& t) {
 		// case: raise the cell to be just lower than its lowest neighbour. This
 		// makes the breaching/tunneling procedures work better.
 		if(h(x, y) < lowest_neighbour) {
-			//operator()(x, y) = nextafter(lowest_neighbour, -2.0 * HeightField::flat/*std::numeric_limits<float>::lowest()*/);
-			operator()(x, y) = lowest_neighbour - 2.0 * HeightField::flat;
+			h(x, y) = lowest_neighbour - 2.f * std::numeric_limits<float>::min();
 		}
 		// Since depressions might have flat bottoms, we treat flats as pits. Mark 
 		// flat/pits as such now.
 		if(h(x, y) <= lowest_neighbour) {
-			pits(x, y) = true;
-			total_pits++; // May not need this
+			at(pits, x, y) = true;
 		}
 	}
 
 	// The Priority-Flood operation assures that we reach pit cells by passing into
 	// depressions over the outlet of minimal elevation on their edge.
 	while (!pq.empty()) {
-		const auto c = pq.top();
+		auto c = pq.top();
 		pq.pop();
 
-		const float elevation = c.first;
-		const Point p = c.second;
+		// float elevation = c.first;
+		Point p = c.second;
 
 		//This cell is a pit: let's consider doing some breaching
-		if(pits(p[0], p[1])) {
+		if(at(pits, p[0], p[1])) {
 			//Locate a cell that is lower than the pit cell, or an edge cell
-			int cc = VertexIndex(p);                                  //Current cell on the path
-			const float pathdepth = -2.0 * HeightField::flat; // std::numeric_limits<float>::lowest(); //Maximum depth found along the path
+			auto cc = at(indexing(pits), p[0], p[1]);                                  //Current cell on the path
+			// float pathdepth = -2.f * std::numeric_limits<float>::lowest(); //Maximum depth found along the path
 			float target_height = h(p[0], p[1]);                 //Depth to which the cell currently being considered should be carved
 
-			if(mode == COMPLETE_BREACHING) {
-				//Trace path back to a cell low enough for the path to drain into it, or
-				//to an edge of the DEM
-				while (cc != NO_BACK_LINK && at(cc) >= target_height) {
-					field[cc] = target_height;
-					cc = backlinks.at(cc);                     //Follow path back
-					//target_height = nextafter(target_height, pathdepth); //Decrease target depth slightly for each cell on path to ensure drainage
-					target_height = target_height - 2.0 * HeightField::flat; //Decrease target depth slightly for each cell on path to ensure drainage
-				}
-			}
-			--total_pits;
-			if(total_pits == 0) {
-				break;
+			//Trace path back to a cell low enough for the path to drain into it, or
+			//to an edge of the DEM
+			while(cc != -1 && at(t.height.values, cc) >= target_height) {
+				at(t.height.values, cc) = target_height;
+				cc = at(backlinks, cc); //Follow path back
+				target_height -= 2.f * std::numeric_limits<float>::min(); //Decrease target depth slightly for each cell on path to ensure drainage
 			}
 		}
 
 		//Looks for neighbours which are either unvisited or pits
 		for(int n = 0; n < DIRECTIONS; n++) {
-			const int px = p[0] + dx[n];
-			const int py = p[1] + dy[n];
+			auto px = p[0] + dx[n];
+			auto py = p[1] + dy[n];
 
-			if(!InsideVertexIndex(px, py)) {
-				continue;
-			}
-			if(visited(px, py) != LindsayCellType::UNVISITED) {
+			if(at(visited, px, py)) {
 				continue;
 			}
 
-			const float my_e = h(px, py);
+			auto my_e = h(px, py);
 
 			//The neighbour is unvisited. Add it to the queue
-			pq.emplace(my_e, Point(px, py));
-			if(fill_depressions) {
-				flood_array.emplace_back(VertexIndex(px, py));
-			}
-			visited(px, py) = LindsayCellType::VISITED;
-			backlinks(px, py) = VertexIndex(p);
-		}
-
-		if(mode != COMPLETE_BREACHING && fill_depressions) {
-			for(const auto f : flood_array) {
-				int parent = backlinks.at(f);
-				if(at(f) <= at(parent)) {
-					field[f] = nextafter(at(parent), std::numeric_limits<float>::max());
-				}
-			}
+			pq.push(std::make_pair(my_e, std::array{px, py}));
+			at(visited, px, py) = true;
+			at(backlinks, px, py) = int(at(indexing(backlinks), p[0], p[1]));
 		}
 	}
 }
