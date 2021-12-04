b5a91336023bf745426cd3e34f227231ba44b2ed
Willy Jacquet <willy.jacquet@outlook.com>
2021-12-04 22:36:39 +0100

    [galin] Stream power erosion extent.

--- galin/src/terrain/algorithm/erosion/stream_power.hpp
+++ galin/src/terrain/algorithm/erosion/stream_power.hpp
@@ -2,20 +2,20 @@
 
 #include "terrain/all.hpp"
 
-struct StreamPowerErosionSettings {
-    float factor = 1.f;
-};
-
 inline
-void stream_power_erosion(Terrain &t) {
+void stream_power_erosion(Terrain &t, float extent) {
     auto da = value_accessor(t.drainage_area);
     auto h = value_accessor(t.height);
     auto s = value_accessor(t.slope);
+
+    auto factor = extent / (
+        upper_bound(t.slope.range)
+        * std::sqrt(upper_bound(t.drainage_area.range)));
 
     auto nx = resolution(t)[0];
     auto ny = resolution(t)[1];
     for(size_t i = 0; i < nx; ++i)
     for(size_t j = 0; j < ny; ++j) {
-        h(i, j) += -0.01f * s(i, j) * std::sqrt(da(i, j));
+        h(i, j) -= factor * s(i, j) * std::sqrt(da(i, j));
     }
 }
