c8ac396a42a8810a80a2ed849a653bbf3b2def56
Willy Jacquet <willy.jacquet@outlook.com>
2021-12-05 15:52:43 +0100

    [galin] Removed two steepest.

--- galin/src/terrain/algorithm/field/drainage_area/all.hpp
+++ galin/src/terrain/algorithm/field/drainage_area/all.hpp
@@ -1,5 +1,4 @@
 #pragma once
 
 #include "steepest.hpp"
-#include "two_steepest.hpp"
 #include "weighted.hpp"
--- galin/src/terrain/algorithm/field/drainage_area/two_steepest.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "terrain/all.hpp"
-
-inline
-void update_drainage_area_using_two_steepest(Terrain& t) {
-    std::ignore = t;
-}
