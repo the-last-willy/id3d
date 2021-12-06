14eb1d5c8d9342285f72727215685cbc54393f2d
Willy Jacquet <willy.jacquet@outlook.com>
2021-12-06 16:52:58 +0100

    [galin] Normalized slope colors.

--- galin/src/terrain/algorithm/field/color/slope.hpp
+++ galin/src/terrain/algorithm/field/color/slope.hpp
@@ -7,8 +7,11 @@ void update_colors_using_slope(Terrain& t) {
     auto c = value_accessor(t.color);
     auto s = value_accessor(t.slope);
 
+    auto extent = upper_bound(t.slope.range);
+
     for(std::size_t i = 0; i < resolution(t)[0]; ++i)
     for(std::size_t j = 0; j < resolution(t)[1]; ++j) {
-        c(i, j) = agl::vec3(1.f / (s(i, j) + 1.f));
+        auto val = s(i, j) / extent;
+        c(i, j) = agl::vec3(val);
     }
 }
